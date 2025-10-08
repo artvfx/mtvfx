ed264a6bdf3af08781f97484fc1d53aa7f2c8b54
ABDOU KHANFRI <99496677+artvfx@users.noreply.github.com>
2025-10-08 02:06:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,19 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    allowedHosts: ['mtvfx.onrender.com'],
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    preview: {
-      port: 10000,
-      allowedHosts: ['mtvfx.onrender.com'], // 👈 أضف هذا السطر
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
+  },
 });
