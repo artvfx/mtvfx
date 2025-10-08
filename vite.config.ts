5ad74acd5e10c14c8fdf5e9a54b7c7704dd331d9
ABDOU KHANFRI <99496677+artvfx@users.noreply.github.com>
2025-10-08 02:18:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  
+  define: {
+  'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+},
   server: {
     port: 3000,
     host: '0.0.0.0',
